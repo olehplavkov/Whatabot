7576ba65652ffec3ddb1e3014691d915bdb4fd27
olehplavkov <108940216+olehplavkov@users.noreply.github.com>
2022-07-11 09:13:08 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+package academy.prog;
+
 import org.telegram.telegrambots.bots.*;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
